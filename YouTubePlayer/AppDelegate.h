8bbcee54108ef70ba2d3435cc87ef884ef4b7c22
djthorpe <djt@mutablelogic.com>
2013-04-27 19:16:54 +0100

    Added skeleton files

---
+++ YouTubePlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  YouTubePlayer
+//
+//  Created by David Thorpe on 27/04/2013.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
